184073c28d52be65f21e95fae9dc72903f0ba376
Jesús Peiró López <jpeirolo@mercadona.es>
2023-11-23 09:02:17 +0100

    Initial xml without recyclerview

--- app/src/main/java/com/jesuspeirolopez/myflexagenda/MainActivity.kt
+++ app/src/main/java/com/jesuspeirolopez/myflexagenda/MainActivity.kt
@@ -1,46 +1,17 @@
 package com.jesuspeirolopez.myflexagenda
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jesuspeirolopez.myflexagenda.ui.theme.MyFlexAgendaTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyFlexAgendaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyFlexAgendaTheme {
-        Greeting("Android")
     }
 }
