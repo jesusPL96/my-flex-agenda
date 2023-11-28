e9a019d63f405ee565020fab7ed0c677ec854f32
Jesús Peiró López <jpeirolo@mercadona.es>
2023-11-28 11:43:12 +0100

    Day, month and year now updates correctly with right and left

--- app/src/main/java/com/jesuspeirolopez/myflexagenda/MainActivity.kt
+++ app/src/main/java/com/jesuspeirolopez/myflexagenda/MainActivity.kt
@@ -4,17 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import com.jesuspeirolopez.myflexagenda.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val calendar: Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.year.setOnClickListener {
+        binding.actualYear.setOnClickListener {
 
             val intent = Intent(this@MainActivity, YearCalendarActivity::class.java)
             startActivity(intent)
@@ -33,5 +37,44 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        binding.dayAfter.setOnClickListener {
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+            updateActualDayTextViews()
+
+        }
+
+        binding.dayBefore.setOnClickListener {
+
+            calendar.add(Calendar.DAY_OF_MONTH, -1)
+            updateActualDayTextViews()
+
+        }
+
+        updateActualDayTextViews()
+
+
+    }
+
+    private fun updateActualDayTextViews() {
+
+        val dayFormat = SimpleDateFormat("dd", Locale.getDefault())
+        val formattedDay = dayFormat.format(calendar.time)
+        binding.actualDay.text = formattedDay
+
+        val monthFormat = SimpleDateFormat("MMMM", Locale("es", "ES"))
+        val formattedMonth = monthFormat.format(calendar.time)
+        binding.actualDay3.text = formattedMonth
+
+        val newYear = calendar.get(Calendar.YEAR)
+        runOnUiThread {
+
+            binding.actualYear.text = newYear.toString()
+        }
+        val yearTextView = binding.actualYear
+
+        if (newYear != calendar.get(Calendar.YEAR)) {
+            yearTextView.text = newYear.toString()
+        }
     }
 }
