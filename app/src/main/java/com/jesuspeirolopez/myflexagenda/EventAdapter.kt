c0adaf776b19aa855c645dd958e42bec36c3343b
Jesús Peiró López <jpeirolo@mercadona.es>
2023-11-29 09:14:39 +0100

    Fixed adapter to update card values

--- app/src/main/java/com/jesuspeirolopez/myflexagenda/EventAdapter.kt
+++ app/src/main/java/com/jesuspeirolopez/myflexagenda/EventAdapter.kt
@@ -3,13 +3,18 @@ package com.jesuspeirolopez.myflexagenda
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class EventAdapter(private val events: List<Event>) : RecyclerView.Adapter<EventAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // Inicializa las vistas dentro de cada elemento de la cuadrícula
-        // Por ejemplo: val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+
+        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        val hourTextView: TextView = itemView.findViewById(R.id.hourTextView)
+        val minuteTextView: TextView = itemView.findViewById(R.id.minuteTextView)
+        val deleteButton: Button = itemView.findViewById(R.id.deleteButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,6 +26,9 @@ class EventAdapter(private val events: List<Event>) : RecyclerView.Adapter<Event
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Configurar las vistas con datos específicos del evento
         val event = events[position]
+        holder.titleTextView.text = event.title
+        holder.hourTextView.text = event.hour
+        holder.minuteTextView.text = event.minute
         // Por ejemplo: holder.titleTextView.text = event.title
     }
 
